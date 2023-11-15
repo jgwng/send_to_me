bfce6193d54b539b67087a9e9e851f7a8f09a297
jgwng <gunwng123@gmail.com>
2023-11-15 22:10:36 +0900

    update :최초 세팅 / 화면 시작시 애니메이션 추가

---
+++ web/common/js/redirect.js
@@ -0,0 +1,20 @@
+function addDelay(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+function openFlutterApp(){
+    window.addEventListener('load', function(ev) {
+      // Download main.dart.js
+      _flutter.loader.loadEntrypoint({
+        serviceWorker: {
+          serviceWorkerVersion: serviceWorkerVersion,
+        },
+        onEntrypointLoaded: async function(engineInitializer) {
+          const appRunner = await engineInitializer.initializeEngine();
+          await addDelay();
+          await appRunner.runApp();
+          document.querySelector("flutter-view").classList.add("fade-in");
+        }
+      });
+    });
+}
